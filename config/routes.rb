e02ed9e52bc296210af065eac10aaf8f51cc54db
Kimiko <kimikomotoyama@gmail.com>
2017-01-05 18:23:18 +0000

    exercise 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'restaurants#index'
-  get 'restaurants/index'
+  get 'restaurants/index', as: 'user_root'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   
 end
