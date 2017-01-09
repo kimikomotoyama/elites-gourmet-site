640285372e2166c702def0ef5ffe73ee0c9a5a52
Kimiko <kimikomotoyama@gmail.com>
2017-01-09 16:37:11 +0000

    checked

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# 10.times do |index|
-#   Faker::Config.locale = :ja
-#   name = Faker::Company.name
-#   telephone_number = Faker::PhoneNumber.phone_number
-#   address = Faker::Address.street_address
-#   has_private_room = Faker::Boolean.boolean
-#   seat_count = Faker::Number.between(10, 100) 
-# end
+30.times do |index|
+  Faker::Config.locale = :ja
+  name = Faker::Company.name
+  telephone_number = Faker::PhoneNumber.phone_number
+  address = Faker::Address.street_address
+  has_private_room = Faker::Boolean.boolean
+  seat_count = Faker::Number.between(10, 100) 
+  
+  image = Faker::Avatar.image
+
+  Restaurant.create(name: name, user_id: 1, telephone_number: telephone_number, address: address, has_private_room: has_private_room,
+  seat_count: seat_count, open_date: DateTime.current.utc)
+  
+  RestaurantImage.create(restaurant_id: index+1, image: image)
+
+end
 
